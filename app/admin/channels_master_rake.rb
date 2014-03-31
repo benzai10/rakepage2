41bc2ec6c631ffe68715538374d9a94375fd5e17
David Antman <semihelix@gmail.com>
2014-03-31 17:43:52 +0900

    Added functionality to add channels to rake from active_admin

---
+++ app/admin/channels_master_rake.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register ChannelsMasterRake do
+  permit_params :master_rake_id, :channel_id
+
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+
+  index do
+    selectable_column
+    id_column
+    column :channel
+    column :master_rake
+    column :created_at
+    column :updated_at
+    actions
+  end
+end
