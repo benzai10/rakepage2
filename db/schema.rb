c6528fd8159ed19a40691edf83d0bc222ee525d6
David Antman <semihelix@gmail.com>
2014-04-01 21:53:30 +0900

    On channel creation get feed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401080917) do
+ActiveRecord::Schema.define(version: 20140401123918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,10 +51,10 @@ ActiveRecord::Schema.define(version: 20140401080917) do
 
   create_table "channels", force: true do |t|
     t.string   "name",         default: "", null: false
-    t.string   "channel_type", default: "", null: false
     t.string   "source",       default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "channel_type"
   end
 
   create_table "channels_master_rakes", force: true do |t|
--- lib/feed_helper.rb
+++ lib/feed_helper.rb
@@ -1,44 +1,58 @@
 module FeedHelper
 
   class Web
+    require 'feedjira'
 
-    def self.get_feeds
+    def self.get_channel_feeds
       urls = []
-      Channel.where(channel_type: "rss").each do |channel|
+      Channel.where(channel_type: 0).each do |channel|
         urls << channel.source
       end
       urls
     end
 
-    def self.process_feed(feeds)
+    def self.process_feeds(feeds)
+      if feeds.nil?
+        p "No  feed is nil!"
+        return
+      end
+
       count = 0
       feeds.each do |url,feed|
-        if feed.respond_to?(:entries)
-          feed.entries.each do |entry|
-            unless Leaflet.where(identifier: entry.entry_id).exists?
-              p entry.entry_id
-              count+=1
-
-              if entry.summary.blank?
-                content = entry.content
-              elsif
-                content = entry.summary
-              end
-              channel = Channel.find_by(source: url)
-              Leaflet.create!(channel_id: channel.id, 
-                              identifier: entry.entry_id,
-                              title: entry.title,
-                              url: entry.url,
-                              author: entry.author,
-                              image: entry.image,
-                              content: content, 
-                              published_at: entry.published)
-            end
-          end
-        end
+        count += create_leaflet(url,feed)
       end
       print count
       print " Leaflets created\n"
+    end
+
+    private
+
+    def self.create_leaflet(url,feed)
+      count = 0
+      if feed.respond_to?(:entries)
+        feed.entries.each do |entry|
+          unless Leaflet.where(identifier: entry.entry_id).exists?
+            p entry.entry_id
+            count+=1
+
+            if entry.summary.blank?
+              content = entry.content
+            elsif
+              content = entry.summary
+            end
+            channel = Channel.find_by(source: url)
+            Leaflet.create!(channel_id: channel.id,
+                            identifier: entry.entry_id,
+                            title: entry.title,
+                            url: entry.url,
+                            author: entry.author,
+                            image: entry.image,
+                            content: content,
+                            published_at: entry.published)
+          end
+        end
+      end
+      count
     end
 
   end
