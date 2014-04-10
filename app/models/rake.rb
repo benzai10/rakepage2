4bc3dfde5839c0c2e25428557d942c8fe600c7fd
David Antman <semihelix@gmail.com>
2014-04-10 13:53:39 +0900

    Autocreate channel on rake creation

--- app/models/rake.rb
+++ app/models/rake.rb
@@ -1,5 +1,5 @@
 class Rake < ActiveRecord::Base
-  after_create :create_heap
+  after_create :create_heap, :create_channel
 
   validates :name, presence: true
   validates :master_rake_id, presence: true
@@ -24,6 +24,10 @@ class Rake < ActiveRecord::Base
 
   def create_heap
     self.heap = Heap.create
+  end
+
+  def create_channel
+    add_channel(Channel.create!(source: id.to_s, name: name, channel_type: 3))
   end
 
 end
