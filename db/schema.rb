f04c25c34074d7979a7ff88ac2a88af01eb9d065
David Antman <semihelix@gmail.com>
2014-04-11 19:41:02 +0900

    Added rake add after signup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140407131527) do
+ActiveRecord::Schema.define(version: 20140411060048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -135,6 +135,7 @@ ActiveRecord::Schema.define(version: 20140407131527) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.datetime "refreshed_at"
   end
 
   create_table "users", force: true do |t|
