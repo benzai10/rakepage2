fb47423da818680f742953b899e58d5b58a98b57
= <steiner.mischa@gmail.com>
2014-06-17 15:12:48 +0900

    changed mailer host in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rakepage2::Application.configure do
     password: ENV["MANDRILL_APIKEY"]
   }
 
-  config.action_mailer.default_url_options = { :host => 'rakepage.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.rakepage.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
