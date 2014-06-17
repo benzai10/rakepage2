7efbfb740cf8c0ff3b92561a73ca7e53d242251c
= <steiner.mischa@gmail.com>
2014-06-17 15:39:36 +0900

    added confirmations_controller for devise

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,16 @@
+class ConfirmationsController < Devise::ConfirmationsController
+  protected
+
+  def after_sign_up_path_for(resource)
+    if resource.is_a?(User)
+      master_rakes_path
+    else
+      super
+    end
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    new_user_session_path
+  end
+end
+
