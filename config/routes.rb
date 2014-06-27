ae8b6cd72c6abe059ef77f42c96e0da10f98b562
= <steiner.mischa@gmail.com>
2014-06-27 19:11:16 +0900

    moved root routing to top in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rakepage2::Application.routes.draw do
-  ActiveAdmin.routes(self)
   root to: "pages#landing"
+  ActiveAdmin.routes(self)
   devise_for :admin_users, ActiveAdmin::Devise.config
   devise_for :users, :controllers => { :registrations => "registrations" }
 
