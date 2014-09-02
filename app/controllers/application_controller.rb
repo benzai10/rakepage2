4f3e95a6775bdce63838d897dcbae27ae1327936
= <steiner.mischa@gmail.com>
2014-09-02 19:47:44 +0900

    changed after_sign_in path to master_rakes index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      myrakes_path
+      master_rakes_path
     else
       super
     end
