b531ddb9ace9b0327ac7e1bb18137b792298bfdc
= <steiner.mischa@gmail.com>
2014-09-15 21:51:50 +0900

    fixed gsub method in leaflet.rb

--- app/models/leaflet.rb
+++ app/models/leaflet.rb
@@ -22,8 +22,8 @@ class Leaflet < ActiveRecord::Base
     content = content.gsub(/img src=http:\/\/a.thumbs.redditmedia.com/, 'img class="reddit-thumbnail" src=http://a.thumbs.redditmedia.com')
     content = content.gsub(/\<img src=default \/\>/, '')
     content = content.gsub(/\<img src=self \/\>/, '')
-    if (content.include? 'width=\"1\"') || (content.include? 'height=\"1\"')
-      content = content.gsub(/<img [\s\S]*?\/>/mi, '')
+    if (content.include? "width=\"1\"") or (content.include? "height=\"1\"")
+      content = content.gsub(/<img [()\s\S]*?\/>/mi, '')
     end
     self.content = content
   end
