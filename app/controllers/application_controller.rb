79b32fe322fe3322cd7d5748b355b1141d7e954b
= <steiner.mischa@gmail.com>
2014-10-05 18:20:46 +0900

    added copy text to myrakes index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
-      master_rakes_path
+      myrakes_path
     else
       super
     end
