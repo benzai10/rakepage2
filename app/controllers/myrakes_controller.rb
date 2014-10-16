ed55dcf94e27a20cdb2726ec4491c333c674ecfe
= <steiner.mischa@gmail.com>
2014-10-16 11:47:20 +0900

    fixed behavior after saving a bookmark in the news feed

--- app/controllers/myrakes_controller.rb
+++ app/controllers/myrakes_controller.rb
@@ -53,12 +53,22 @@ class MyrakesController < ApplicationController
     end
     # -- This part has to be more refined because it creates duplicates...
     if user_signed_in?
-      @added_leaflets = Leaflet.where("id IN (?)", MasterHeapLeafletMap.where("master_heap_id IN (?) AND created_at > ?", @rake.master_rake.master_heaps.pluck(:id), current_user.last_sign_in_at).pluck(:leaflet_id))
+      @added_leaflets = Leaflet.where("id IN (?)", 
+                                MasterHeapLeafletMap.where("master_heap_id IN (?) AND created_at > ?", 
+                                                     @rake.master_rake.master_heaps.pluck(:id), 
+                                                     current_user.last_sign_in_at).pluck(:leaflet_id) -
+                                HeapLeafletMap.where("heap_id IN (?)",
+                                                     @rake.heaps.pluck(:id)).pluck(:leaflet_id))
     else
-      @added_leaflets = Leaflet.where("id IN (?)", MasterHeapLeafletMap.where("master_heap_id IN (?) AND created_at > ?", @rake.master_rake.master_heaps.pluck(:id), Time.now - 1.week).pluck(:leaflet_id))
+      @added_leaflets = Leaflet.where("id IN (?)", 
+                                MasterHeapLeafletMap.where("master_heap_id IN (?) AND created_at > ?", 
+                                                     @rake.master_rake.master_heaps.pluck(:id),
+                                                     Time.now - 1.week).pluck(:leaflet_id))
     end
     @feed_leaflets = @rake.feed_leaflets("news", params[:refresh]).order("published_at DESC").page(params[:page]).per(50)
-    @feed_leaflets -= @added_leaflets
+    #@feed_leaflets -= Leaflet.where("id IN (?)",
+    #                          HeapLeafletMap.where("heap_id IN (?)",
+    #                                                 @rake.heaps.pluck(:id)).pluck(:leaflet_id))
     @leaflet_types = CategoryLeafletTypeMap.where(category_id: @rake.master_rake.category_id).pluck(:leaflet_type_id)
     missing_leaflet_types = @leaflet_types - @rake.heaps.pluck(:leaflet_type_id)
     missing_leaflet_types.each do |mlt|
