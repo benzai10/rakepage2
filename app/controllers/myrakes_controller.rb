de249b95d761c5e1a3dbc846245cb31698da6cbd
= <steiner.mischa@gmail.com>
2014-11-27 12:39:48 +0900

    fixed bug when copying bookmarks to new rake and got rid of second rendering of news feed in master rakes

--- app/controllers/myrakes_controller.rb
+++ app/controllers/myrakes_controller.rb
@@ -114,7 +114,7 @@ class MyrakesController < ApplicationController
           @heap_leaflets_maps = MasterHeapLeafletMap.where("master_heap_id IN (?)", master_rake.master_heaps.pluck(:id))
           @heap_leaflets_maps.each do |hl|
             leaflet = Leaflet.find(hl.leaflet_id)
-            @rake.add_leaflet(leaflet, hl.master_heap.leaflet_type_id, leaflet.title, hl.leaflet_desc, nil)
+            @rake.add_leaflet(leaflet, hl.master_heap.leaflet_type_id, leaflet.title, hl.leaflet_desc, "", "", "", 0, 0, 0)
           end
         end
         redirect_to myrake_path(@rake, view: "news", refresh: "yes")
