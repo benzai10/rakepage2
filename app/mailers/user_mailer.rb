9914b35edfd6d6544a087dea3e109e3d473c987d
= <steiner.mischa@gmail.com>
2015-01-05 18:37:57 +0900

    welcome email no rakes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "rakepage@rakepage.com"
-  def welcome_email
-    @user = User.find(11)
+  def welcome_email_no_rake(user)
+    @user = user
     @url = "http://rakepage.com"
-    mail(to: "steiner.mischa@gmail.com",
+    mail(to: @user.email,
          subject: "Test mail",
          template_path: "user_mailer",
-         template_name: "welcome_email")
+         template_name: "welcome_email_no_rake")
     headers['X-MC-Track'] = "opens, clicks"
   end
 end
