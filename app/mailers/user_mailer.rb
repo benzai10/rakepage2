540e2708c3ecb0f45ca8dcd1ae29f2cdfa041360
= <steiner.mischa@gmail.com>
2015-01-05 16:32:44 +0900

    testing user mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ActionMailer::Base
   default from: "rakepage@rakepage.com"
   def welcome_email
+    @user = User.find(11)
+    @url = "http://rakepage.com"
     mail(to: "steiner.mischa@gmail.com",
          subject: "Test mail",
          template_path: "user_mailer",
