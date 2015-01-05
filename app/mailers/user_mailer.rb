3fa8c0b985a9194c3fcaba308de401881f108978
= <steiner.mischa@gmail.com>
2015-01-05 16:29:26 +0900

    testing user mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "rakepage@rakepage.com"
   def welcome_email
-    mail(:to => "steiner.mischa@gmail.com", :subject => "Test mail", :body => "Test mail body")
+    mail(to: "steiner.mischa@gmail.com",
+         subject: "Test mail",
+         template_path: "user_mailer",
+         template_name: "welcome_email")
   end
 end
