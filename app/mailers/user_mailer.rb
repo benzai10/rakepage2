a48a7fd2ace4bd8473cec6190fc0ba11b228a99a
= <steiner.mischa@gmail.com>
2015-01-05 19:08:41 +0900

    added reminder_email_no_rake

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,4 +9,15 @@ class UserMailer < ActionMailer::Base
          template_name: "welcome_email_no_rake")
     headers['X-MC-Track'] = "opens, clicks"
   end
+
+  def reminder_email_no_rake(user)
+    @user = user
+    @url = "http://rakepage.com"
+    mail(to: @user.email,
+         subject: "Hi #{@user.username}, master your priorities using Rakepage",
+         template_path: "user_mailer",
+         template_name: "reminder_email_no_rake")
+    headers['X-MC-Track'] = "opens, clicks"
+  end
+
 end
