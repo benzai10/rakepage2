0ee6cc5444ed8e4477576637c9af1bb7bfb290bb
= <steiner.mischa@gmail.com>
2015-01-06 12:39:21 +0900

    added welcome_email_with_rake

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,16 @@ class UserMailer < ActionMailer::Base
     headers['X-MC-Track'] = "opens, clicks"
   end
 
+  def welcome_email_with_rake(user)
+    @user = user
+    @url = "http://rakepage.com"
+    mail(to: @user.email,
+         subject: "Hi #{@user.username}, master your priorities using Rakepage",
+         template_path: "user_mailer",
+         template_name: "welcome_email_with_rake")
+    headers['X-MC-Track'] = "opens, clicks"
+  end
+
   def reminder_email_no_rake(user)
     @user = user
     @url = "http://rakepage.com"
