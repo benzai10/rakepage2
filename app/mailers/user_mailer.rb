30d6acd81611aba0ca79269b5c81fc2e654eda84
= <steiner.mischa@gmail.com>
2015-01-13 22:05:40 +0900

    changes in welcome emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -14,7 +14,7 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url = "http://rakepage.com"
     mail(to: @user.email,
-         subject: "Hi #{@user.username}, master your priorities using Rakepage",
+         subject: "Hi #{@user.username}, stay focused and be awesome with Rakepage",
          template_path: "user_mailer",
          template_name: "welcome_email_no_rake")
     headers['X-MC-Track'] = "opens, clicks"
@@ -24,7 +24,7 @@ class UserMailer < ActionMailer::Base
     @user = user
     @url = "http://rakepage.com"
     mail(to: @user.email,
-         subject: "Hi #{@user.username}, master your priorities using Rakepage",
+         subject: "Hi #{@user.username}, stay focused and be awesome with Rakepage",
          template_path: "user_mailer",
          template_name: "welcome_email_with_rake")
     headers['X-MC-Track'] = "opens, clicks"
