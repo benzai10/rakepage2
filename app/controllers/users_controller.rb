a0eb0db4065850c3db1aa874fda6d1332c193e6e
= <steiner.mischa@gmail.com>
2015-01-31 20:37:43 +0900

    fixed bug if timezone can't be read

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,15 @@ class UsersController < ApplicationController
         @top_rakes = Myrake.where(user_id: current_user.id, top_rake: 1)
         @other_rakes = Myrake.where(user_id: current_user.id, top_rake: 0)
         @committed_action_steps = History.where(user_id: current_user.id, history_int: 1)
-        @today_committed_action_steps = @committed_action_steps.where("created_at >= ?", Time.find_zone(cookies[:timezone]).now.beginning_of_day)
-        @committed_action_goals = History.where(user_id: current_user.id, history_int: 2)
-        @today_committed_action_goals = @committed_action_goals.where("created_at >= ?", Time.find_zone(cookies[:timezone]).now.beginning_of_day)
+        if !Time.find_zone(cookies[:timezone]).nil?
+          @today_committed_action_steps = @committed_action_steps.where("created_at >= ?", Time.find_zone(cookies[:timezone]).now.beginning_of_day)
+          @committed_action_goals = History.where(user_id: current_user.id, history_int: 2)
+          @today_committed_action_goals = @committed_action_goals.where("created_at >= ?", Time.find_zone(cookies[:timezone]).now.beginning_of_day)
+        else
+          @today_committed_action_steps = @committed_action_steps.where("created_at >= ?", Time.now.beginning_of_day)
+          @committed_action_goals = History.where(user_id: current_user.id, history_int: 2)
+          @today_committed_action_goals = @committed_action_goals.where("created_at >= ?", Time.now.beginning_of_day)
+        end
         @notifications = Notification.all.order(published_at: :desc)
         @master_rakes_count = MasterRake.all.count
         if params[:view] == "top5"
